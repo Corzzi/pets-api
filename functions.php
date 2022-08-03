431080bcf478d4d832722d29eaae0c40cbb0f940
murilothom <murilothom.2002@hotmail.com>
2022-08-02 21:04:15 -0300

    fixing

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ function change_api() {
 }
 add_filter('rest_url_prefix', 'change_api');
 
-function exepire_token() {
+function expire_token() {
   return time() + (60 * 60 * 24);
 }
 add_action('jwt_auth_expire', 'expire_token');
